1f36ccb4c0f5f2314aa07e729ac830114343116c
DanilSizz <theghost.teenager@gmail.com>
2024-11-05 16:28:26 +0300

    upd

---
+++ PC_Config/Components/Component.py
@@ -0,0 +1,13 @@
+class Component:
+
+    self.name = None
+    self.price = None
+    self.specs = None
+
+    def get_info(self):
+        return (self.name, self.price, self.specs)
+
+    def set_info(self):
+        pass
+
+    
---
+++ PC_Config/PersonalComputer.py
@@ -0,0 +1,19 @@
+class PersonalC:
+
+    def __init__(self):
+        #Материнская плата
+        self.mother = None
+        # Процессор
+        self.cpu = None
+        # Графический процессор
+        self.gpu = None
+        #Оперативная память
+        self.ram = None
+        #Постоянная память 
+        self.rom = None
+        #Блок питания
+        self.psu = None
+        #Кулер процессорный
+        self.cooler = None 
+        #Корпус
+        self.case = None
